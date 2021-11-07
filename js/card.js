1a9ebacef45ef0a78858b4cff37c19d939e9ba1c
jpropato <julianpropato@gmail.com>
2021-11-07 13:10:36 -0300

    Cards as new object

--- js/card.js
+++ js/card.js
@@ -1,5 +1,25 @@
 var cards = [
-    new Card ('Samsung Galaxy S21+', '$150.000', 'img/productos/samsung-galaxy-s21-plus-5g.jpg')
+    new Card ('Samsung Galaxy S21+', '$150.000', 'img/productos/samsung-galaxy-s21-plus-5g.jpg'),
+    new Card ('Samsung Galaxy J7', '$45.000', 'img/productos/celular-samsung.jpg'),
+    new Card ('Motorola Moto E7', '$45.000', 'img/productos/motorola-moto-e7.jpg'),
+    new Card ('Apple Iphone 12', '$190.000', 'img/productos/apple-iphone-12.jpg'),
+    new Card ('Apple Iphone 11 Pro', '$210.000', 'img/productos/apple-iphone-11-pro-max.jpg'),
+    new Card ('Samsung Galaxy S20 FE', '$69.000', 'img/productos/s20\ fe.jpg'),
+    new Card ('Motorola G60s', '$49.000', 'img/productos/g60s.jpg'),
+    new Card ('Samsung Galaxy A32', '$26.000', 'img/productos/a32.jpg'),
+    new Card ('Samsung Galaxy A72', '$78.000', 'img/productos/a72.jpg'),
+    new Card ('Motorola Moto G9 Plus', '$36.000', 'img/productos/g9-plus.jpg'),
+    new Card ('Celular LG LK420-HM', '$16.000', 'img/productos/lgk420hm.jpg'),
+    new Card ('Celular LG LK510-HM', '$25.000', 'img/productos/lgk510hm.jpg'),
+    new Card ('Motorola Lenovo G9', '$33.000', 'img/productos/moto-g9-play.jpg'),
+    new Card ('Celular ZTE Blade A3', '$13.000', 'img/productos/zte-blase-a3.jpg'),
+    new Card ('Celular TCL L17', '$18.000', 'img/productos/tcll7+.jpg'),
+    new Card ('Celular TCL L10 SE', '$15.000', 'img/productos/tcl10se.jpg'),
+    new Card ('Motorola Moto G100', '$89.000', 'img/productos/g100.jpg'),
+    new Card ('Samsung Galaxy A01', '$14.000', 'img/productos/A01-CORE.jpg'),
+    new Card ('Samsung Galaxy A02', '$16.000', 'img/productos/A02.jpg'),
+    new Card ('Samsung Galaxy Fold Z', '$350.000', 'img/productos/fold.jpg'),
+    new Card ('Samsung Galaxy A12', '$15.000', 'img/productos/a12.jpg'),
 ]
 
 var elemCardsContainer = document.getElementsByClassName('cards-container')[0]
@@ -13,6 +33,24 @@ function Card (header, price, image){
         var card = document.createElement('a')
         card.classList.add('card')
         card.href = '#'
+
+        var that = this
+
+        card.addEventListener('click', function(){
+            ev.preventDefault()
+            console.log(this)
+            this.classList.toggle('card--seleccionada')
+
+            // INICIO DE CHINO AVANZADO
+            var nuevoItemCarrito = document.createElement('div')
+            nuevoItemCarrito.classList.add('carrito__item')
+            nuevoItemCarrito.innerHTML = that.heading /*THAT?*/
+            console.log(nuevoItemCarrito)
+            
+            // elemSectionCarrito: variable global
+            elemSectionCarrito.append(nuevoItemCarrito)
+            // FIN DE CHINO AVANZADO
+        })
 
         card.innerHTML = 
         `<article class="card__article">
---
+++ js/carrito.js
@@ -0,0 +1,4 @@
+var btnCarrito = document.getElementsByClassName('search-bar__carrito-container')[0]
+btnCarrito.addEventListener('click', function () {
+    elemSectionCarrito.classList.toggle('section-carrito--visible')
+})
